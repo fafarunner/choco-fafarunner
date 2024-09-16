b551fa0a0a93de652fdc5358f60bf918e44d1b9d
kjxbyz <kjxbyz888@outlook.com>
2024-09-16 04:42:43 +0000

    Apply Choco package update - fafarunner 1.0.0.358
    Signed-off-by: kjxbyz <kjxbyz888@outlook.com>

--- _auto/fafarunner/tools/chocolateyinstall.ps1
+++ _auto/fafarunner/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/fafarunner/fafarunner/releases/download/v1.0.0%2B335/fafarunner_1.0.0+335_windows_x64.exe'
+$url64      = 'https://github.com/fafarunner/fafarunner/releases/download/v1.0.0%2B358/fafarunner_1.0.0%2B358_windows_x64.exe'
 
 $arch = [System.Environment]::Is64BitOperatingSystem
 
@@ -18,7 +18,7 @@ $packageArgs = @{
 
   softwareName  = 'fafarunner*'
 
-  checksum64    = '01712c545c77399c63e6feb64f94653e6f8cc92b5be2cf8575c6c4a1294ca2b3'
+  checksum64    = '2a5e5a2f376012f8d91daae93a92154f1dad4548f1a2e8a5ae94b167a21dc715'
   checksumType64= 'sha256'
 
   silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
